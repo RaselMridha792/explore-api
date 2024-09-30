960389e7051cfecf6a2d1f58739850736371539f
rasel mridha <raselmridha792@gmail.com>
2024-09-30 21:34:22 +0600

    get the userdata value by using fetch and display the user name and emails by using for loop and innerText

--- js/index.js
+++ js/index.js
@@ -2,12 +2,14 @@ function getData() {
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(Response => Response.json())
         .then(data => getUser(data))
-
+        
 
     function getUser(data){
+        const ul = document.getElementById('user-list')
         for(user of data){
-            console.log(user.username);
-            console.log(user.email);
+            const li = document.createElement('li');
+            li.innerText = `${user.name} is your new user and his email is: ${user.email}`;
+            ul.appendChild(li);
         }
     }
 }
